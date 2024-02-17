7d7d8dcef28b9aeb30f06d8178b115ca7a0bc6eb
vdumas <valentin3135@gmail.com>
2024-02-17 12:28:46 +0100

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
+import '../styles/App.css';
+import listings from '../data/listings.json';
 
 function App() {
+  console.log(listings);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
