ac51e966951c1a80e16fc4d71d9ad642df8e9b7a
vdumas <valentin3135@gmail.com>
2024-02-23 17:19:59 +0100

    Création complète de la Home Page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import bannerImage1 from '../styles/assets/bannerImage1.jpg';
+import HousingCard from '../components/HousingCard';
+import listings from '../data/listings.json';
+import '../styles/HomePage.scss'
 
 const HomePage = () => {
   return (
     <>
       <Banner image={bannerImage1} title="Chez vous, partout et ailleurs"/>
+      <div className="gallery">
+      {listings.map(listing => (
+        <HousingCard
+          key={listing.id}
+          id={listing.id}
+          title={listing.title}
+          cover={listing.cover}
+        />
+      ))}
+    </div>
     </>
   );
 };
